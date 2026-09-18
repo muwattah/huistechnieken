e2a0363b3401cf79b5eb55d0044dd24582d67529
muwattah <elabd214o@gmail.com>
2026-09-18 23:55:41 +0200

    Disallow all indexing (noindex)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,6 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://www.huistechnieken.be/sitemap.xml",
+    rules: { userAgent: "*", disallow: "/" },
   };
 }
